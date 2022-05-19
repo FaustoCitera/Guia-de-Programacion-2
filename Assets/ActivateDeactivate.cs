30ca49d220672ff21c5a260ea440fba5ebc46ff8
FaustoCitera <101577508+FaustoCitera@users.noreply.github.com>
2022-05-19 09:01:22 -0300

    Initial commit

---
+++ Assets/ActivateDeactivate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActivateDeactivate : MonoBehaviour
+{
+	public GameObject inputField;
+
+	public void ActivateAndDeactivate()
+	{
+		//if (inputField.activeInHierarchy)
+		//{
+		//	inputField.SetActive(false);
+		//}
+		//else
+		//{
+		//	inputField.SetActive(true);
+		//}
+		inputField.SetActive(!inputField.activeInHierarchy);
+	}
+}
